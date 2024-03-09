04f2cf18ce085508c142c339b1b6719c60c3dbee
stephanocastillo <iamaxlstephano@gmail.com>
2024-03-09 18:33:54 +0100

    client.c almost done

--- client.c
+++ client.c
@@ -1,18 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: axcastil <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 21:04:40 by axcastil          #+#    #+#             */
-/*   Updated: 2024/01/16 21:04:42 by axcastil         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
-#include "funtion_collection/ft_printf.h"
+int confirmation;
+
+static void	send_char(char c, pid_t pid)
+{
+	int	i; 
+	
+	i = 7;
+	while(i >= 0)
+	{
+		confirmation = 0;
+		if(kill(pid, 0) < 0)
+			exit(1);
+		if(c & (1 << i))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i--;
+		//crear bucle para confirmacion de envío con **variable global**
+		while(confirmation != 1)
+			sleep(1);
+	}
+}
+
+static void	send_str(char *str, pid_t pid)
+{
+	while(*str)
+	{
+		send_char(*str, pid);
+		str++;
+	}
+	send_char('\0', pid);
+}
+
+static void	signal_handler(int signal)
+{
+	if(signal == SIGUSR1)
+		confirmation = 1;
+	else if(signal == SIGUSR2)
+		exit(0);
+}
 
 int main(int argc, char **argv)
 {
-	char hola;
+	pid_t pid;
+
+	if (argc != 3)
+	{
+		printf("¡ARGUMENTS ARE MISSING :3");
+		return(1);
+	}
+	signal(SIGUSR1, &signal_handler);
+	signal(SIGUSR2, &signal_handler);
+	pid = atoi(argv[1]);
+	send_str(argv[2], pid);
+	while(1)
+		sleep(1);
 }

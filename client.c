1848acf629f5c9aa3179358b601184b2c43c0d7f
stephanocastillo <iamaxlstephano@gmail.com>
2024-05-13 15:34:41 +0200

    done

--- client.c
+++ client.c
@@ -6,41 +6,57 @@
 /*   By: axcastil <axcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 17:22:02 by axcastil          #+#    #+#             */
-/*   Updated: 2024/05/04 17:22:03 by axcastil         ###   ########.fr       */
+/*   Updated: 2024/05/13 15:20:18 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./42_collection/ft_printf.h"
-#include "./42_collection/libft.h"
+#include "42_collection/ft_printf.h"
+#include "42_collection/libft.h"
 
-void	send_str(char *str, pid_t pid, int len)
+int	g_confirmation;
+
+void	send_char(char letter, pid_t pid)
 {
-	int	i;
 	int	bit;
 
-	i = 0;
-	if (*str == 0)
+	bit = __CHAR_BIT__ * sizeof(char) - 1;
+	while (bit >= 0)
 	{
-		ft_printf("empty string!!\n");
-		exit(1);
-	}
-	while (i < len)
-	{
-		bit = __CHAR_BIT__ * sizeof(char) - 1;
-		while (bit >= 0)
-		{
-			if ((str[i] >> bit) & 1)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			bit--;
-			usleep(50);
-		}
-		i++;
+		g_confirmation = 0;
+		if ((letter >> bit) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		bit--;
+		while (g_confirmation != 1)
+			usleep(10);
 	}
 }
 
-// this works fine
+
+void	send_str(char *str, pid_t pid)
+{
+	int	cur;
+
+	cur = 0;
+	while (str[cur])
+	{
+		send_char(str[cur], pid);
+		cur++;
+	}
+	send_char('\n', pid);
+	send_char(0, pid);
+}
+	
+
+void	sig_usr_handler(int signal)
+{
+	if (signal == SIGUSR1)
+		g_confirmation = 1;
+	else if (signal == SIGUSR2)
+		exit(0);
+}
+
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
@@ -50,9 +66,10 @@ int	main(int argc, char **argv)
 		ft_printf("¡ARGUMENTS ARE MISSING :3\n");
 		return (1);
 	}
-	pid = atoi(argv[1]);
-	argv[2] = ft_strjoin(argv[2], "\n");
-	send_str(argv[2], pid, ft_strlen(argv[2]));
-	free(argv[2]);
-	return (0);
+	signal(SIGUSR1, &sig_usr_handler);
+	signal(SIGUSR2, &sig_usr_handler);
+	pid = ft_atoi(argv[1]);
+	send_str(argv[2], pid);
+	while(1)
+		sleep(1);
 }
--- server.c
+++ server.c
@@ -6,33 +6,50 @@
 /*   By: axcastil <axcastil@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 17:21:54 by axcastil          #+#    #+#             */
-/*   Updated: 2024/05/04 17:21:55 by axcastil         ###   ########.fr       */
+/*   Updated: 2024/05/13 15:34:05 by axcastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./42_collection/ft_printf.h"
 #include "./42_collection/libft.h"
+#include <signal.h>
+#include <stdlib.h>
 
-void	sig_usr(int sig)
+void	sig_handler(int signal, siginfo_t *pid, void *opcional)
 {
 	static char	letter;
-	static int	number;
-	static int	bit;
+	static int	bit = -1;
+	static int	sigusr;
 
-	if (number == 0)
-		bit = 7;
-	if (sig == SIGUSR1)
-		letter |= (1 << bit);
-	else if (sig == SIGUSR2)
-		letter &= ~(1 << bit);
-	number++;
+	(void)opcional;
+	if (kill(pid->si_pid, 0) < 0)
+		exit(1);
+	if ((bit < 0))
+		bit = __CHAR_BIT__ * sizeof(char) - 1;
+	if (signal == SIGUSR1)
+		sigusr = 1;
+	else if (signal == SIGUSR2)
+		sigusr = 0;
+	letter = (letter << 1) + sigusr;
+	if (!bit && letter)
+		ft_putchar_fd(letter, 1);
+	else if (!bit && !letter)
+		kill(pid->si_pid, SIGUSR2);
 	bit--;
-	if (number == 8)
-	{
-		write(1, &letter, 1);
-		letter = 0;
-		number = 0;
-	}
+	kill(pid->si_pid, SIGUSR1);
+}
+
+void	sigaction_set(int signal, void (*signal_handler)(int, siginfo_t *, void *))
+{
+	struct sigaction susr;
+
+	susr.sa_sigaction = signal_handler;
+	susr.sa_flags = SA_SIGINFO | SA_RESTART | SA_NODEFER;	
+	sigemptyset(&susr.sa_mask);
+	if (signal == SIGUSR1)
+		sigaction(SIGUSR1, &susr, 0);
+	else if(signal == SIGUSR2)
+		sigaction(SIGUSR2, &susr, 0);
 }
 
 int	main(void)
@@ -47,10 +64,8 @@ int	main(void)
 	ft_printf("██║░╚═╝░██║██║██║░╚███║██║░░░██║░░░██║░░██║███████╗██║░╚██╗\n");
 	ft_printf("╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n");
 	ft_printf("\t\t\tpid: %d\n", pid);
+	sigaction_set(SIGUSR1, &sig_handler);
+	sigaction_set(SIGUSR2, &sig_handler);
 	while (1)
-	{
-		signal(SIGUSR1, &sig_usr);
-		signal(SIGUSR2, &sig_usr);
 		sleep(1);
-	}
 }
